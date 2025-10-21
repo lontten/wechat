73fa2fe71a00914571d0bf12b2ca508e8eced422
lontten <lontten@163.com>
2025-10-21 16:44:09 +0800

    添加 获取稳定版接口调用凭据

--- miniprogram/access_token.go
+++ miniprogram/access_token.go
@@ -20,12 +20,31 @@ func (p MiniProgramConfig) GetAccessToken() (GetAccessTokenResp, error) {
 
 	return lcore.Get[GetAccessTokenResp](url)
 }
+
+// GetStableAccessToken 获取稳定版接口调用凭据
+// 获取小程序全局唯一后台接口调用凭据，token有效期为7200s，开发者需要进行妥善保存
+// forceRefresh 默认使用 false。
+// 1. force_refresh = false 时为普通调用模式，access_token 有效期内重复调用该接口不会更新 access_token；
+// 2. 当force_refresh = true 时为强制刷新模式，会导致上次获取的 access_token 失效，并返回新的 access_token
+// https://developers.weixin.qq.com/miniprogram/dev/OpenApiDoc/mp-access-token/getStableAccessToken.html
+func (p MiniProgramConfig) GetStableAccessToken(forceRefresh ...bool) (GetAccessTokenResp, error) {
+	url := "https://api.weixin.qq.com/cgi-bin/stable_token"
+	url += "&grant_type=" + "client_credential"
+	url += "?appid=" + p.Appid
+	url += "&secret=" + p.Secret
+	if len(forceRefresh) > 0 && forceRefresh[0] {
+		url += "&force_refresh=true"
+	}
+
+	return lcore.Get[GetAccessTokenResp](url)
+}
+
 func (p MiniProgramConfig) GetAccessTokenCache() (string, error) {
 	v, ok := p.Get(p.CacheKeyAccessToken())
 	if ok {
 		return v.(string), nil
 	}
-	token, err := p.GetAccessToken()
+	token, err := p.GetStableAccessToken()
 	if err != nil {
 		return "", err
 	}
