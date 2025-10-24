3535b51f030898a8d5b163cdb0a187703acd13ff
lontten <lontten@163.com>
2025-10-24 16:23:13 +0800

    🐛 添加初始化参数

--- cache/core.go
+++ cache/core.go
@@ -15,6 +15,7 @@ type MemberCache struct {
 func InitMemberCache() MemberCache {
 	// 初始化缓存：配置最大成本（可理解为最大内存占用估算）、缓冲区大小等
 	c, err := ristretto.NewCache(&ristretto.Config{
+		NumCounters: 1e7,     // number of keys to track frequency of (10M).
 		MaxCost:     1 << 30, // 最大成本（示例：1GB，根据实际需求调整）
 		BufferItems: 64,      // 缓冲区大小（影响写入性能，建议 64-256）
 	})
