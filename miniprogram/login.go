59dab735365b212129494a310533e06905f95af9
lontten <lontten@163.com>
2025-10-24 16:12:28 +0800

    优化Code2Session接口参数

--- miniprogram/login.go
+++ miniprogram/login.go
@@ -4,10 +4,6 @@ import (
 	"github.com/lontten/lcore/v2"
 )
 
-type Code2SessionReq struct {
-	JsCode    string `json:"jsCode"`    // 登录时获取的 code，可通过wx.login获取
-	GrantType string `json:"grantType"` // 授权类型，此处只需填写 authorization_code
-}
 type Code2SessionResp struct {
 	SessionKey string `json:"session_key"` // 会话密钥
 	UnionID    string `json:"unionid"`     // 用户在开放平台的唯一标识符，若当前小程序已绑定到微信开放平台帐号下会返回。
@@ -18,12 +14,12 @@ type Code2SessionResp struct {
 
 // Code2Session 登录凭证校验。通过 wx.login 接口获得临时登录凭证 code 后传到开发者服务器调用此接口完成登录流程
 // https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/login/auth.code2Session.html
-func (p MiniProgramConfig) Code2Session(req Code2SessionReq) (Code2SessionResp, error) {
+func (p MiniProgramConfig) Code2Session(code string) (Code2SessionResp, error) {
 	url := "https://api.weixin.qq.com/sns/jscode2session"
 	url += "?appid=" + p.Appid
 	url += "&secret=" + p.Secret
-	url += "&js_code=" + req.JsCode
-	url += "&grant_type=" + req.GrantType
+	url += "&js_code=" + code
+	url += "&grant_type=" + "authorization_code"
 
 	return lcore.Get[Code2SessionResp](url)
 }
